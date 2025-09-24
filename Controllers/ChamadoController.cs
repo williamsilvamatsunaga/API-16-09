8ec542c2e7ba7cba3a0fc0ed0e5224c7fac7d461
William da Silva Matsunaga <williamsilvamatsunaga@outlook.com>
2025-09-23 20:35:05 -0400

    Segundo commit

--- Controllers/ChamadoController.cs
+++ Controllers/ChamadoController.cs
@@ -22,9 +22,24 @@ namespace ApiServico.Controllers
         private static int _proximoId = 3;
 
         [HttpGet]
-        public async Task<IActionResult> BuscarTodos()
+        public async Task<IActionResult> BuscarTodos(
+            [FromQuery] string? search,
+            [FromQuery] string? situacao)
         {
-            var chamados = await _context.Chamados.ToListAsync();
+            var query = _context.Chamados.AsQueryable();
+            
+
+            if(search is not null)
+            {
+                query = query.Where(x => x.Titulo.Contains(search));
+            }
+
+            if(situacao is not null)
+            {
+                query = query.Where(x => x.Status.Equals(situacao));
+            }
+
+            var chamados = await query.ToListAsync();
 
             return Ok(chamados);
         }
--- Models/Duo/ChamadoDuo.cs
+++ Models/Duo/ChamadoDuo.cs
@@ -1,8 +1,10 @@
-﻿namespace ApiServico.Models.Duo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiServico.Models.Duo
 {
     public class ChamadoDuo
     {
-
+        [Required(ErrorMessage = "O título é obrigatório")]
         public required string Titulo { get; set; }
 
         public required string Descricao { get; set; }
